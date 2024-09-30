eacb87c31ffcda771252cab46a82620bf4d1ad7d
Pekka K <k.pekka@protonmail.com>
2024-09-30 10:09:49 +0200

    fix: remove finding by matching when looking for neowarrior window

--- lua/neowarrior/NeoWarrior.lua
+++ lua/neowarrior/NeoWarrior.lua
@@ -551,7 +551,7 @@ function NeoWarrior:focus()
   for _, handle in ipairs(windows) do
     local buf_handle = vim.api.nvim_win_get_buf(handle)
     local buf_name = vim.api.nvim_buf_get_name(buf_handle)
-    if string.find(buf_name, "neowarrior") then
+    if buf_name == "neowarrior" then
       vim.api.nvim_set_current_win(handle)
       return true
     end
