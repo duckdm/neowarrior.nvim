6ef5f74dcf49b3d16b406e0b33d26f4a6ab135e0
Pekka K <k.pekka@protonmail.com>
2024-10-01 10:28:27 +0200

    fix: toggling task/project floats does not work when using same key for both

--- lua/neowarrior/NeoWarrior.lua
+++ lua/neowarrior/NeoWarrior.lua
@@ -995,6 +995,44 @@ function NeoWarrior:set_keymaps()
     end, default_keymap_opts)
   end
 
+  local task_float_key = self.config.task_float.enabled or nil
+  local project_float_key = self.config.project_float.enabled or nil
+
+  --- Show task float
+  if type(task_float_key) == "string" then
+    vim.keymap.set("n", self.config.task_float.enabled, function()
+      local uuid = self.buffer:get_meta_data('uuid')
+      self:close_floats()
+      if not self.task_float then
+        self:open_task_float()
+      else
+        self.task_float = nil
+      end
+      if project_float_key == task_float_key and (not uuid) then
+        if not self.project_float then
+          self:open_project_float()
+        else
+          self.project_float = nil
+        end
+      end
+    end, default_keymap_opts)
+  end
+
+  --- Show project float
+  if task_float_key ~= project_float_key and type(project_float_key) == "string" then
+    vim.keymap.set("n", self.config.project_float.enabled, function()
+      local uuid = self.buffer:get_meta_data('uuid')
+      self:close_floats()
+      if not uuid then
+        if not self.project_float then
+          self:open_project_float()
+        else
+          self.project_float = nil
+        end
+      end
+    end, default_keymap_opts)
+  end
+
   -- Back to list/refresh
   if self.config.keys.back then
     vim.keymap.set("n", '<Esc>', function() self:list() end, default_keymap_opts)
