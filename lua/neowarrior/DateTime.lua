e06d6c345f9968f920928e282e5d90b32f3cf092
Pekka K <k.pekka@protonmail.com>
2024-10-02 09:28:25 +0200

    feat: implement tieske/date library
    https://github.com/Tieske/date

--- lua/neowarrior/DateTime.lua
+++ lua/neowarrior/DateTime.lua
@@ -1,155 +1,105 @@
+local date = require('tieske.date')
+
 ---@class DateTime
 ---@field date string
----@field year number
----@field month number
----@field day number
----@field hour number
----@field minute number
----@field second number
----@field timestamp number
+---@field year number?
+---@field month number?
+---@field day number?
+---@field hour number?
+---@field minute number?
+---@field second number?
+---@field timestamp number?
 ---@field offset number
----@field new fun(self: DateTime, date: string): DateTime
----@field parse fun(self: DateTime, str: string): number
+---@field new fun(self: DateTime, date: string|nil): DateTime
+---@field parse fun(self: DateTime, str: string|nil): number
 ---@field format fun(self: DateTime, format: string): string|osdate
 ---@field default_format fun(self: DateTime): string|osdate
 ---@field relative fun(self: DateTime): string
 local DateTime = {}
 
 --- Create new datetime
----@param date string
+---@param input string|nil
 ---@return DateTime
-function DateTime:new(date)
+function DateTime:new(input)
 
   local datetime = {}
   setmetatable(datetime, self)
 
   self.__index = self
 
-  datetime.date = date
-  datetime.year = 0
-  datetime.month = 0
-  datetime.day = 0
-  datetime.hour = 0
-  datetime.minute = 0
-  datetime.second = 0
-  datetime.timestamp = self:parse(date)
-  datetime.offset = 0
+  datetime.date = date(input)
 
   return datetime
-end
-
---- Parse date
----@param str string
----@return number Timestamp
-function DateTime:parse(str)
-
-  local year, month, day, hour, min, sec = str:match '(%d%d%d%d)(%d%d)(%d%d)T(%d%d)(%d%d)(%d%d)Z'
-  self.year = year
-  self.month = month
-  self.day = day
-  self.hour = hour
-  self.minute = min
-  self.second = sec
-  self.offset = ((60 * 60) * _Neowarrior.config.time_offset)
-
-  local utc_time = os.time {
-    year = self.year,
-    month = self.month,
-    day = self.day,
-    hour = self.hour,
-    min = self.minute,
-    sec = self.second,
-    isdst = false, -- Explicitly state that this is not daylight saving time
-  }
-
-  local local_time = os.date('*t', utc_time)
-  local_time.isdst = os.date('*t').isdst
-
-  self.timestamp = os.time(local_time)
-  -- FIX: need a better solution for this
-  self.timestamp = self.timestamp + self.offset
-
-  return self.timestamp
 end
 
 --- Format date
 ---@param format string
 ---@return string|osdate
 function DateTime:format(format)
-  return os.date(format, self.timestamp)
+  return self.date:fmt(format)
 end
 
 --- Get default formatted date
 ---@return string|osdate
 function DateTime:default_format()
-  if self.timestamp == 0 then
-    return ''
-  end
-  return os.date('%Y-%m-%d, %H:%M', self.timestamp)
+  return self:format('%Y-%m-%d, %H:%M')
 end
 
 --- Function to calculate the time difference
----@return number
+---@return number Diff in seconds
 function DateTime:diff()
-  local target_time = self:parse(self.date)
-  local now = os.time()
-  local diff = os.difftime(target_time, now)
-  return diff
+  local now = DateTime:new(nil)
+  return date.diff(self.date, now.date):spanseconds()
+end
+
+--- Function to calculate the time difference
+---@return dateObject
+function DateTime:diff_object()
+  local now = DateTime:new(nil)
+  return date.diff(self.date, now.date)
 end
 
 -- Function to calculate the relative time difference
 ---@return string
 function DateTime:relative()
 
-  local diff = self:diff()
-  local negative = false
+  local diff = self:diff_object()
+  local years = math.ceil(diff:spandays()) and math.ceil(diff:spandays() / 365) or 0
+  local months = math.ceil(diff:spandays()) and math.ceil(diff:spandays() / 30) or 0
+  local days = math.ceil(diff:spandays()) and math.ceil(diff:spandays()) or 0
+  local hours = math.ceil(diff:spanhours()) and math.ceil(diff:spanhours()) or 0
+  local minutes = math.ceil(diff:spanminutes()) and math.ceil(diff:spanminutes()) or 0
+  local seconds = diff:spanseconds()
 
-  if diff < 0 then
-    negative = true
-    diff = diff * -1
+  if years > 1 or years < 0 then
+    return years .. "y"
   end
 
-  local days = math.floor(diff / (24 * 60 * 60))
-  local hours = math.floor((diff % (24 * 60 * 60)) / (60 * 60))
-  local minutes = math.floor((diff % (60 * 60)) / 60)
-
-  local value = ''
-
-  if days > 0 or days < -1 then
-    if days >= 30 or days <= -30 then
-      local months = math.floor(days / 30)
-      value = string.format('~%dmon', months)
-    else
-      value = string.format('%dd', days)
-    end
-  elseif hours > 0 or hours < -1 then
-    value = string.format('%dh', hours)
-  elseif minutes > 0 or minutes < 0 then
-    value = string.format('%dm', minutes)
-  else
-    value = 'now'
-  end
-  if negative then
-    value = '-' .. value
+  if months > 1 or months < 0 then
+    return months .. "mon"
   end
 
-  return value
+  if days > 1 or days < 0 then
+    return days .. "d"
+  end
+
+  if hours > 1 or hours < 0 then
+    return hours .. "h"
+  end
+
+  if minutes > 1 or minutes < 0 then
+    return minutes .. "m"
+  end
+
+  return seconds .. "s"
+
 end
 
 --- Get relative hours
 --- @return number
 function DateTime:relative_hours()
-
-  local diff = self:diff()
-  local negative = false
-
-  if diff < 0 then
-    negative = true
-    diff = diff * -1
-  end
-
-  local value = diff / (60 * 60)
-  return (not negative) and value or value * -1
+  local diff = self:diff_object()
+  return diff:spanhours()
 end
 
 return DateTime
